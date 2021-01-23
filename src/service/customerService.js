7a27b96b0bc65e8fb1ca178547116a039735b150
Giovanny Palacios <gpalacios@unicauca.edu.co>
2021-01-23 18:36:54 -0500

    Add last month parameters to get customer orders

--- src/service/customerService.js
+++ src/service/customerService.js
@@ -3,5 +3,7 @@ const apiURL = '1.0/customers'
 
 export default {
   list: (page, size) => api.get(apiURL, { params: { page, size } }),
-  get: (id) => api.get(`${apiURL}/${id}`)
+  get: (id) => api.get(`${apiURL}/${id}`),
+  orders: (id, page, size, startDate, endDate) =>
+    api.get(`${apiURL}/${id}/orders`, { params: { page, size, startDate, endDate } })
 }
